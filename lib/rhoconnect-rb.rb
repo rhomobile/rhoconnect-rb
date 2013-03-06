5abbf1ae662006aa8086ec6bed03abb4006409bf
lucas campbell-rossen <lucas@rhomobile.com>
2013-03-06 11:58:23 -0800

    add api paths instead of dir loop

--- lib/rhoconnect-rb.rb
+++ lib/rhoconnect-rb.rb
@@ -6,4 +6,11 @@ require 'rhoconnectrb/client'
 require 'rhoconnectrb/resource'
 require 'rhoconnectrb/endpoints'
 require 'rhoconnectrb/railtie' if defined?(Rails)
-Dir["/rhoconnectrb/api/*.rb"].each {|file| require file }
+require 'rhoconnectrb/api/base'
+require 'rhoconnectrb/api/clients'
+require 'rhoconnectrb/api/read_state'
+require 'rhoconnectrb/api/resource'
+require 'rhoconnectrb/api/sources'
+require 'rhoconnectrb/api/store'
+require 'rhoconnectrb/api/system'
+require 'rhoconnectrb/api/users'
